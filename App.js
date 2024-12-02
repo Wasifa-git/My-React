0e13f7964704e8c18717384109ef9c87b0b5fcd8
wasifa aktar <wasifa.git.acc@gmail.com>
2024-12-02 12:52:08 +0530

    first push

---
+++ App.js
@@ -0,0 +1,15 @@
+/**<div id="parent" >
+    <div id="child">
+        <h1>i m h1</h1>
+    </div>
+  </div> **/
+  const parent = React.createElement("div",{id:"parent"}, // 3rd argument is child to be passed
+    React.createElement("div",{id:"child"},
+        React.createElement("h1",{},"i m h1"
+    
+        )
+    )
+  );
+const header =React.createElement("h1",{},"hello I am react");
+     const root = ReactDOM.createRoot(document.getElementById("root"));
+     root.render(parent);
