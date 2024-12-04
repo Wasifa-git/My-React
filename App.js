b0727161ef50d7c74a757f32cc199e386d104055
wasifa aktar <wasifa.git.acc@gmail.com>
2024-12-04 14:21:26 +0530

    dom hierarchy with react

--- App.js
+++ App.js
@@ -3,6 +3,8 @@
         <h1>i m h1</h1>
     </div>
   </div> **/
+  import React from "react";
+  import ReactDOM from "react-dom";
   const parent = React.createElement("div",{id:"parent"}, // 3rd argument is child to be passed
     React.createElement("div",{id:"child"},
         React.createElement("h1",{},"i m h1"
